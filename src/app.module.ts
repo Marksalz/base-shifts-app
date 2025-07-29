a98db43e95abc9a090690de4a462d5677f231467
Mark <menachemsalzberg13@gmail.com>
2025-07-29 15:24:32 +0300

    feat: add ConfigModule and update entity models; implement user, shift and assignment entities with relationships

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,13 +10,17 @@ import { AssignmentsModule } from './assignments/assignments.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'base_shifts_app',
+      database: process.env.DB_NAME || 'shiftsapp',
       autoLoadModels: true,
       synchronize: true,
     }),
--- src/shifts/entities/shift.entity.ts
+++ src/shifts/entities/shift.entity.ts
@@ -1,1 +1,36 @@
-export class Shift {}
+import { Column, Model, Table, DataType, HasMany } from 'sequelize-typescript';
+import { Assignment } from '../../assignments/entities/assignment.entity';
+
+@Table({
+    tableName: 'shifts',
+    timestamps: true,
+})
+export class Shift extends Model {
+    // @Column({
+    //     type: DataType.INTEGER,
+    //     primaryKey: true,
+    //     autoIncrement: true,
+    // })
+    // declare id: number;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+    })
+    declare startTime: Date;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+    })
+    declare endTime: Date;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare location: string;
+
+    @HasMany(() => Assignment)
+    declare assignments: Assignment[];
+}
