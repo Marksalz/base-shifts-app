1c90f3b58da711cf23f9aa16fb10d4bdd4a5f48c
Mark <menachemsalzberg13@gmail.com>
2025-07-29 11:40:01 +0300

    feat: implement CreateUserDto with validation and user role enum; update UsersController routes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsString, IsEnum, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export enum UserRole {
+    SOLDIER = 'soldier',
+    COMMANDER = 'commander'
+}
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    password: string;
+
+    @IsEnum(UserRole)
+    role: UserRole;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,29 +5,29 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
-  @Post()
+  @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+  @Get('read')
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
+  @Get('read:id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('update:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete('delete:id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
