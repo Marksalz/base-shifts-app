fc44405b7d80f22e48b842b2fb3f72159291a242
Mark <menachemsalzberg13@gmail.com>
2025-07-29 11:12:38 +0300

    feat: add class-transformer and class-validator dependencies; implement validation in SignupDto and main application

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,10 +1,19 @@
+import { IsString, IsEnum, IsNotEmpty } from 'class-validator';
+
 export enum UserRole {
     SOLDIER = 'soldier',
     COMMANDER = 'commander'
 }
 
 export class SignupDto {
+    @IsString()
+    @IsNotEmpty()
     username: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
+
+    @IsEnum(UserRole)
     role: UserRole;
 }
